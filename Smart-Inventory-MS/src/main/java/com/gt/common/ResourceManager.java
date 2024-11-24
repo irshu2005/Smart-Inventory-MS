77e630481a6096187bdfe309eed017d59839ae9e
Irshad Ahamad <irshu79054@gmail.com>
2024-11-24 23:00:33 +0530

    PNG

--- Smart-Inventory-MS/src/main/java/com/gt/common/ResourceManager.java
+++ Smart-Inventory-MS/src/main/java/com/gt/common/ResourceManager.java
@@ -7,7 +7,9 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,10 +17,9 @@ import java.util.Map;
 /**
  * com.gt.common-ResourceManager.java<br/>
  *
- * @author Irshad Ahamad @@ irshu79054@gmail.com <br/>
- * Created on : Nov 1, 2024<br/>
- * Copyright : <a
- * Irshad Ahamad </a>
+ * @author Irshad
+ * <p>
+ * Created on: Nov 1, 2024<br/>
  */
 public class ResourceManager {
 
@@ -26,6 +27,12 @@ public class ResourceManager {
     private static final String a = "gt?Pass,e#. ";
     private static Map<String, String> stringConstantsMap;
 
+    /**
+     * Fetch a string constant by key from the resource file.
+     *
+     * @param key the key of the string constant.
+     * @return the corresponding value or null if not found.
+     */
     public static synchronized String getString(String key) {
         if (stringConstantsMap == null) {
             try {
@@ -37,6 +44,14 @@ public class ResourceManager {
         return stringConstantsMap.get(key);
     }
 
+    /**
+     * Reads a map of key-value pairs from a file.
+     *
+     * @param file   the file to read.
+     * @param isEncry true if the file is encrypted.
+     * @return the map of key-value pairs.
+     * @throws IOException if an error occurs while reading the file.
+     */
     public static Map<String, String> readMap(String file, boolean isEncry) throws IOException {
         BufferedReader in = new BufferedReader(new FileReader(file));
         Map<String, String> map = new HashMap<>();
@@ -53,30 +68,68 @@ public class ResourceManager {
         return map;
     }
 
+    /**
+     * Fetch an image from the resources folder.
+     *
+     * @param fileName the name of the image file.
+     * @return the Image or null if not found.
+     */
     public static Image getImage(String fileName) {
-        // src/image/
-        return readImage(ResourceManager.class.getResource("/images/" + fileName).toString());
+        URL resource = ResourceManager.class.getResource("/images/" + fileName);
+        if (resource == null) {
+            System.err.println("Image resource not found: /images/" + fileName);
+            return null;
+        }
+        return new ImageIcon(resource).getImage();
     }
 
+    /**
+     * Fetch an ImageIcon from the resources folder.
+     *
+     * @param resourcePath the path of the image file relative to the resources folder.
+     * @return the ImageIcon or an empty icon if not found.
+     */
     public static ImageIcon getImageIcon(String resourcePath) {
-        return new ImageIcon(ResourceManager.class.getResource("/images/" + resourcePath));
+        URL resource = ResourceManager.class.getResource("/images/" + resourcePath);
+        if (resource == null) {
+            System.err.println("ImageIcon resource not found: /images/" + resourcePath);
+            return new ImageIcon(); // Return an empty icon to prevent null pointer
+        }
+        return new ImageIcon(resource);
     }
 
+    /**
+     * Read a BufferedImage from the resources folder.
+     *
+     * @param imageName the name of the image file.
+     * @return the BufferedImage or null if not found.
+     */
     public static BufferedImage readImage(String imageName) {
         try {
-            File input = new File(imageName);
-            BufferedImage image = ImageIO.read(input);
-            return image;
+            URL resource = ResourceManager.class.getResource("/images/" + imageName);
+            if (resource == null) {
+                System.err.println("BufferedImage resource not found: /images/" + imageName);
+                return null;
+            }
+            return ImageIO.read(resource);
         } catch (IOException ie) {
-            System.out.println("Error:" + ie.getMessage());
+            System.err.println("Error reading image: " + ie.getMessage());
         }
         return null;
     }
 
+    /**
+     * Test the image loading functionality.
+     */
     public static void main(String[] args) {
-        ImageIcon ic = new ImageIcon(ResourceManager.class.getResource("/images/logout-on.png"));
-        ImageIcon ic2 = getImageIcon("logout-on.png");
-        System.out.println(ic.getIconHeight());
-        System.out.println(ic2.getIconHeight());
+        String[] testImages = {"logout-on.png", "nonexistent.png"};
+        for (String img : testImages) {
+            ImageIcon icon = getImageIcon(img);
+            if (icon.getIconWidth() == -1) {
+                System.err.println("Failed to load image: " + img);
+            } else {
+                System.out.println("Successfully loaded image: " + img);
+            }
+        }
     }
 }
